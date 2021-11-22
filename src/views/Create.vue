a745dde7164fed5c059405c3277ad635f8b417b8
delaquash <olaidemmanuel1@gmail.com>
2021-11-22 09:02:04 +0100

    fixed UI for workout type and workout name

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,6 +1,56 @@
 <template>
-  <div>
-    <h1>Welcome Emmanuel</h1>
+  <div class="max-w-screen-md mx-auto px-4 py-10">
+    <!-- status message -->
+    <div class="mb-10 p-4 bg-light-grey rounded-md">
+      <p class="text-at-light-green">
+        {{ statusMsg}}
+      </p>
+      <p class="text-red-500">
+        {{ errorMsg }}
+      </p>
+    </div>
+
+    <!-- create  -->
+    <div class="p-8 flex items-start bg-light-grey rounded-md shadow-lg">
+      <!-- form -->
+      <form class="flex flex-col gap-y-5 w-full">
+        <h1 class="text-2xl text-at-light-green">
+          Record Workout
+        </h1>
+        <div class="flex flex-col">
+          <label for="workout-name" class="mb-1 text-sm text-at-light-screen">
+            Workout Name
+          </label>
+          <input
+            type="text"
+            class="p-2 text-gray-500 focus:outline-none"
+            id="workout-name"
+            v-model="workoutName"
+            required
+          >
+
+          <!-- workout Type -->
+          <div class="flex flex-col">
+            <label for="workout-type" class="mb-1 text-sm text-at light-green">
+              Workout Type
+            </label>
+            <select
+              id="workout-type"
+              class="p-2 text-gray-500 focus:outline-none"
+              required
+              v-model="workoutType"
+            >
+            <option value="select-workout">Select Workout</option>
+            <option value="strength">Strength Training</option>
+            <option value="cardio">Cardio</option>
+            </select>
+          </div>
+        </div>
+
+        <!-- Strength training inputs -->
+        
+      </form>
+    </div>
   </div>
 </template>
 
