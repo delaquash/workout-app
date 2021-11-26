49215f80f6fb2331d8123eb819c7d7d9877b4446
delaquash <olaidemmanuel1@gmail.com>
2021-11-26 11:05:22 +0100

    fixed homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,8 @@
             Looks empty here .....
           </h1>
           <router-link
-            :to="{name: 'create'}"
-            class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+            :to="{name: 'Create'}"
+            class="mt-6 py-2 px-6 rounded-sm text-sm
                 text-white bg-at-light-green duration-200 border-solid
                   border-2 border-transparent hover:border-at-light-green
                 hover:bg-white hover:text-at-light-green"
@@ -24,11 +24,28 @@
           v-for="(workout, index) in data"
           :key="index"
         >
+        <!-- Cardio Img -->
         <img
           v-if="workout.workoutType === 'cardio'"
           src="@/assets/images/running-light-green.png"
           class="h-24 w-auto"
-          alt="">
+          alt=""
+        />
+
+          <!-- Strength Training -->
+        <img
+          v-else
+          src="@/assets/images/dumbbell-light-green.png"
+          class="h-24 w-auto"
+          alt=""
+        />
+
+        <p class="mt-6 py-1 px-3 text-xs text-white bg-at-light-green shadow-md rounded-lg">
+          {{ workout.workoutType }}
+        </p>
+        <h1 class="mt-8 mb-2 text-center text-xl text-at-light-green">
+          {{ workout.workoutName }}
+        </h1>
 
         </router-link>
       </div>
